caabe6a8b46203c681926cc14bd455a0235bfc34
Talib Khan <talib12sp8star@gmail.com>
2026-06-26 21:30:02 +0530

    Add Solution class to find median of sorted arrays
    Implemented a class with a method to find the median of two sorted arrays by merging and sorting them.

--- Find Median of Sorted Array/python3.py
+++ Find Median of Sorted Array/python3.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        merged_list = nums1 + nums2
+        merged_list.sort()
+        
+        if (len(merged_list) % 2 == 1):
+            return merged_list[(len(merged_list)//2)]
+            
+        else:
+            val1 = merged_list[(len(merged_list)//2)-1]
+            val2 = merged_list[(len(merged_list)//2)]
+            return (val1 + val2)/2
