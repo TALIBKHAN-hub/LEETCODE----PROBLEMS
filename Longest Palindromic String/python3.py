19d05783e6a0d31213e3e24a617b88592be655c9
Talib Khan <talib12sp8star@gmail.com>
2026-06-27 22:38:19 +0530

    Implement longest palindrome substring finder

--- Longest Palindromic String/python3.py
+++ Longest Palindromic String/python3.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        def expand(s,l,r):
+            while(l>=0 and r < len(s) and s[l] == s[r]):
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        
+        result = ""
+        
+        for i in range(len(s)):
+            odd = expand(s,i,i)
+            even = expand(s,i,i+1)
+        
+            if len(odd) > len(result):
+                result = odd
+            if len(even) > len(result):
+                result = even
+        
+        return result
